24a9bcbc5542915fac3938b28496c4f97102e775
Jonathan Wight <jwight@mac.com>
2025-02-17 16:02:12 -0800

    Initial source.

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+// // use std::path::PathBuf;
+
+// // use jj_lib::repo::{RepoLoader, StoreFactories};
+// // use jj_lib::settings::UserSettings;
+// // use jj_lib::config::{StackedConfig, ConfigSource};
+// // use jj_lib::workspace::WorkingCopyFactories;
+// // use jj_lib::local_working_copy::{LocalWorkingCopy, LocalWorkingCopyFactory};
+
+// // fn main() {
+// //     let repo_path = PathBuf::from(".jj/repo");
+// //     let config = StackedConfig::with_defaults();
+// //     let settings = UserSettings::from_config(config).unwrap();
+// //     let store_factories = StoreFactories::default();
+// //     let loader = RepoLoader::init_from_file_system(&settings, &repo_path, &store_factories).unwrap();
+// //     let repo = loader.load_at_head().unwrap();
+// //     let store = repo.index_store();
+// // }
+
+// use jj_cli::Repo;
+
+fn main() {
+    //     let path = std::path::PathBuf::from(".");
+    //     let repo = Repo::new(path);
+    //     let logs = repo.log().unwrap();
+    //     for log in logs {
+    //         println!("{:?}", log);
+    //     }
+}
